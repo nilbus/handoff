1502bbb0ad42dc9aca6e121a678bbfb3b8f7742c
Edward Anderson <nilbus@nilbus.com>
2015-02-18 08:43:13 -0500

    Rename SessionsController to WelcomeController

--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < ApplicationController
-  def index
-    
-  end
-
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "sessions#index"
+  root "welcome#index"
 end
