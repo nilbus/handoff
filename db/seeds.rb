bb4bf7fd4e81653a6301c91f7c8fe905e60770e9
Edward Anderson <nilbus@nilbus.com>
2015-02-28 14:08:05 -0800

    Create demo users as seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.find_or_create_by(email: 'dr-peters@cardio.med')  { |user| user.password = 'test' }
+User.find_or_create_by(email: 'dr-johnson@pcp.local')  { |user| user.password = 'test' }
+User.find_or_create_by(email: 'dr-erickson@pulmo.med') { |user| user.password = 'test' }
