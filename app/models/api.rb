b90d40f3c3bc1ff513e08254bad00cef5747fa37
Edward Anderson <nilbus@nilbus.com>
2015-03-17 22:20:00 -0400

    Remove a comment that has been incorporated into working code

--- app/models/api.rb
+++ app/models/api.rb
@@ -126,13 +126,4 @@ class API
     end
   end
 
-  # http://stackoverflow.com/questions/1820451/ruby-style-how-to-check-whether-a-nested-hash-element-exists
-  #class Hash
-    #def dig(*path)
-      #path.inject(self) do |location, key|
-        #location.respond_to?(:keys) ? location[key] : nil
-      #end
-    #end
-  #end
-
 end
