09d534f17ebf33d389441842e4601e7fd66b067e
Kyle McIntyre <kylejmcintyre@gmail.com>
2015-03-25 00:21:12 -0600

    Basic ability to share a handoff with another user

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,8 +1,19 @@
 class SharesController < ApplicationController
   def new
   end
+
   def create
+    @share = Share.new(share_params)
+    @share.save()
+    @handoff = Handoff.find(share_params['handoff_id'])
+    redirect_to @handoff
   end
+
   def destroy
+  end
+
+  def share_params
+    # TODO we don't actually insure that the person creating the share has access to the handoff!
+    params.require(:share).permit(:handoff_id, :user_id)
   end
 end
