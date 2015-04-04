a94cce7a77635fe0bb61b5b7a0ecf917e8b0085d
Edward Anderson <nilbus@nilbus.com>
2015-04-04 14:35:36 -0400

    Remove unused parent_id column
    In the end, we only list annotations for an object sequentially,
    rather than allowing replies to individual annotations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404183124) do
+ActiveRecord::Schema.define(version: 20150404183139) do
 
   create_table "annotations", force: :cascade do |t|
     t.string   "resource_id"
-    t.integer  "parent_id"
     t.integer  "type"
     t.text     "content"
     t.datetime "created_at",  null: false
