b52b4060e57fed78624e8ec2b31a073ffb7449b7
Jonathan Young <jyoung@groupon.com>
2015-04-07 16:07:08 -0500

    added functionality to prevent individuals who didn't have a share on a handoff to view a handoff

--- app/controllers/handoffs_controller.rb
+++ app/controllers/handoffs_controller.rb
@@ -7,6 +7,7 @@ class HandoffsController < ApplicationController
     @handoff = Handoff.find(params[:id])
     @annotations_by_resource = @handoff.annotations.sequential.group_by(&:resource_id)
     @patient_data = Patient.new(@handoff.patient_id)
+    raise ActiveRecord::RecordNotFound, 'You do not have access to this handoff' unless @handoff.shares.map(&:user_id).include?(current_user.id)
   end
 
   def create
