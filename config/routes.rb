699f72fa6c424b46380fa7bff7031342ccbec6a3
Corey Featherly <corey.f@live.com>
2015-04-17 15:28:24 -0700

    Replace login page redirect with home page redirect

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   resources "annotations", only: [:create, :update, :destroy]
   resources "shares", only: [:create, :destroy]
 
+  #override devise's new_user_registration path before it is set by devise so that unsigned in attempts to access protected pages redirect the user to the app root
+  get '/users/sign_in', to: redirect('/'), as: 'new_user_registration'
+
   devise_for :users
 
   root "welcome#index"
 
   get 'about'   => 'welcome#about'
   get 'contact' => 'welcome#contact'
+
+
   
 end
